eea5f33c068179d2abd277f8d36b3039dfb33ba3
samzapata <legionario.zapata@gmail.com>
2020-11-27 18:08:17 -0500

    improve looking

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,17 @@
 console.log("Developed by @samzapata");
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("scrollmenu-options");
+var content = document.getElementById("projectTabContent");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+    content.style.marginTop = "5rem";
+  } else {
+    header.classList.remove("sticky");
+    content.style.marginTop = "0";
+  }
+}
